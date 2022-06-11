2bdc371dfe14094610b166208f8a92af0a4396fd
Lalo9706 <antony_0000@hotmail.com>
2022-06-11 18:45:27 -0500

    actualización pantallas AhorcadoJugador y AhorcadoRetador

--- JuegoArcado/AhorcadoRetador.xaml.cs
+++ JuegoArcado/AhorcadoRetador.xaml.cs
@@ -8,9 +8,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Media;
+//using JuegoArcado.AhorcadoSprites;
 
 namespace JuegoArcado
 {
@@ -19,9 +22,27 @@ namespace JuegoArcado
     /// </summary>
     public partial class AhorcadoRetador : Window
     {
+
         public AhorcadoRetador()
         {
+            
             InitializeComponent();
+            String direccion = "ahorcadoSprite2.png";
+
+            BitmapImage sprite = new BitmapImage();
+            sprite.BeginInit();
+            sprite.UriSource = new Uri(direccion, UriKind.Relative);
+            sprite.EndInit();
+            spriteAhorcado.Source = sprite;
+
+        }
+
+        private void clickBtnIncorrecto(object sender, RoutedEventArgs e)
+        {
+            lbProgresoPalabra.Content = "- - - - - - - - -";
+            //< Image x: Name = "spriteAhorcado" Source = "/ahorcado_sprite_1.png" HorizontalAlignment = "Left" Height = "338" Width = "338" Margin = "302,0,0,0" Grid.Row = "1" VerticalAlignment = "Top" />
+            //        <Rectangle x:Name="spriteAhorcado" HorizontalAlignment="Left" Height="338" Width="338" Margin="302,0,0,0" Grid.Row="1" Stroke="Black" VerticalAlignment="Top"/>
+            //"C:/Users/ThinkPad/Source/Repos/Samytheturtle/Juego-Arcado-Supemo/JuegoArcado/ImagenesJuegos/AhorcadoSprites/ahorcado_sprite_7.png"
         }
     }
 }
