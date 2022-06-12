bc6d6661e0265ce7ad80d42c2ecb3f446c9db527
esteb <esteb@DESKTOP-UB4OERL>
2022-06-12 14:13:12 -0500

    Cambio Wcf

---
+++ ServicioArcado/ServiceAhorcado.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServicioArcado
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServiceAhorcado" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione ServiceAhorcado.svc o ServiceAhorcado.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class ServiceAhorcado : IServiceAhorcado
+    {
+        
+    }
+}
